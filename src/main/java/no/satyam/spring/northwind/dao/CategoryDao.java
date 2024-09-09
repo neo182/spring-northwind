c80a5403e8d63a1d27b54a42b6483a9eec0f1632
satya <meetneo1@gmail.com>
2024-09-09 23:00:58 +0200

    Added a method to add products in the given category

--- src/main/java/no/satyam/spring/northwind/dao/CategoryDao.java
+++ src/main/java/no/satyam/spring/northwind/dao/CategoryDao.java
@@ -1,6 +1,7 @@
 package no.satyam.spring.northwind.dao;
 
 import no.satyam.spring.northwind.domain.Category;
+import no.satyam.spring.northwind.domain.Product;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -8,6 +9,14 @@ public class CategoryDao extends AbstractHibernateDao<Category> {
 
 	public CategoryDao() {
 		setClazz(Category.class);
+	}
+
+	public void addProductsInCategory(Category category, Product... products) {
+		for (Product product : products) {
+			category.getProducts().add(product);
+		}
+
+		sessionFactory.getCurrentSession().persist(category);
 	}
 
 }
--- src/test/java/no/satyam/spring/northwind/dao/CategoryDaoTest.java
+++ src/test/java/no/satyam/spring/northwind/dao/CategoryDaoTest.java
@@ -2,8 +2,8 @@ package no.satyam.spring.northwind.dao;
 
 import no.satyam.spring.northwind.config.HibernateConfig;
 import no.satyam.spring.northwind.domain.Category;
+import no.satyam.spring.northwind.domain.Product;
 import no.satyam.spring.northwind.testutil.TestDataProvider;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +11,8 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -27,15 +29,9 @@ class CategoryDaoTest {
 	@Autowired
 	private TestDataProvider dataProvider;
 
-	private Category category;
-
-	@BeforeEach
-	public void setUp() {
-		category = dataProvider.getCategory();
-	}
-
 	@Test
 	public void shouldDoCRUDOperationsWithApplicationUser() {
+		Category category = dataProvider.getCategory();
 		// Create
 		Category persistedCategory = categoryDao.create(category);
 		assertThat(persistedCategory.getId(), is(notNullValue()));
@@ -57,6 +53,26 @@ class CategoryDaoTest {
 		categoryDao.deleteById(categoryById.getId());
 		Category deletedCategory = categoryDao.findOne(categoryById.getId());
 		assertThat(deletedCategory, is(nullValue()));
+	}
+
+	@Test
+	public void shouldAddProductsInACategory() {
+		Category category = dataProvider.getCategory();
+		Product product1 = dataProvider.getProduct();
+
+		// Add product1 in a new category
+		categoryDao.addProductsInCategory(category, product1);
+
+		Category categoryWithOneProduct = categoryDao.findOne(category.getId());
+		List<Product> products = categoryWithOneProduct.getProducts();
+		assertThat(products.size(), is(1));
+
+		// Add product2 in an existing category
+		Product product2 = dataProvider.getProduct();
+		categoryDao.addProductsInCategory(category, product2);
+		Category categoryWithTwoProducts = categoryDao.findOne(category.getId());
+		products = categoryWithTwoProducts.getProducts();
+		assertThat(products.size(), is(2));
 	}
 
 }
